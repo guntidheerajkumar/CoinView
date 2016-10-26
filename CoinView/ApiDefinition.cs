64ec4d1ff3ed6bb307400a21e7ecd4f3d54b6783
Dheeraj Kumar G <guntidheerajkumar@gmail.com>
2016-10-26 13:11:36 +0530

    Coin View
    Coin View Flip Animation.

---
+++ CoinView/ApiDefinition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using UIKit;
+using Foundation;
+using ObjCRuntime;
+using CoreGraphics;
+
+namespace CoinView
+{
+	// @interface CMSCoinView : UIView
+	[BaseType(typeof(UIView))]
+	interface CMSCoinView
+	{
+		// -(id)initWithPrimaryView:(UIView *)view1 andSecondaryView:(UIView *)view2 inFrame:(CGRect)frame;
+		[Export("initWithPrimaryView:andSecondaryView:inFrame:")]
+		IntPtr Constructor(UIView view1, UIView view2, CGRect frame);
+
+		// @property (retain, nonatomic) UIView * primaryView;
+		[Export("primaryView", ArgumentSemantic.Retain)]
+		UIView PrimaryView { get; set; }
+
+		// @property (retain, nonatomic) UIView * secondaryView;
+		[Export("secondaryView", ArgumentSemantic.Retain)]
+		UIView SecondaryView { get; set; }
+
+		// @property float spinTime;
+		[Export("spinTime")]
+		float SpinTime { get; set; }
+	}
+}
---
+++ CoinViewSample/ViewController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CoinView;
+using UIKit;
+using CoreGraphics;
+
+namespace CoinViewSample
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			var view1 = new UIView();
+			var view2 = new UIView();
+
+			view1.BackgroundColor = UIColor.Yellow;
+			view2.BackgroundColor = UIColor.Blue;
+
+			this.View.AddSubview(view2);
+			this.View.AddSubview(view1);
+
+
+			//button1.TouchUpInside += (sender, e) => {
+			CMSCoinView coinView = new CMSCoinView(view1, view2, new CGRect(30, 30, this.View.Frame.Width - 60, 150));
+			coinView.PrimaryView = view1;
+			coinView.SecondaryView = view2;
+			coinView.SpinTime = 1.0f;
+			this.View.AddSubview(coinView);
+			//};
+		}
+
+		public override void ViewWillAppear(bool animated)
+		{
+			base.ViewWillAppear(animated);
+		}
+	}
+}
